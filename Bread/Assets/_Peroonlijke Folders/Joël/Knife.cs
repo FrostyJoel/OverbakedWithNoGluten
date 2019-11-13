7d8fd3101607e9af6861ccd5201a0d969e4fdaa2
JelledeBue <jelle@debue.nl>
2019-11-13 20:41:33 +0100

    Some stuff
    Tomatoe = big

---
+++ Bread/Assets/_Peroonlijke Folders/Joël/Knife.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public float force;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Cuttable")
+        {
+            Vector3 cuttingForce = new Vector3(other.transform.position.x,0, 0);
+            other.GetComponent<Rigidbody>().AddForce(cuttingForce * force);
+        }
+    }
+}
