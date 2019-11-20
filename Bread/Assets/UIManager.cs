c32c1a35927849f150fa1fb017ff818170b47ae9
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2019-11-20 11:09:59 +0100

    Temp UI

---
+++ Bread/Assets/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text time;
+    public Text order;
+    public OrderManager orderMan;
+    private void Start()
+    {
+        orderMan = GameObject.FindGameObjectWithTag("ReceptManager").GetComponent<OrderManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        time.text = "Time Left: "; //Add TimeLeft
+        if(orderMan.orderList.Count > 0)
+        {
+            order.text = "Next Order: " + "\n" + orderMan.orderList.Peek().name.ToString();
+        }
+        else
+        {
+            order.text = "Next Order: " + "\n" + "No Orders";
+        }
+    }
+}
